d14fb00b9e99a7d2f9b357d40563ebaba36d0e30
danielcsar <89037218+danielcsar@users.noreply.github.com>
2021-09-28 18:20:41 -0300

    Add files via upload

---
+++ atividades/Imovel.java
@@ -0,0 +1,30 @@
+package atividades;
+
+public class Imovel {
+	int codimovel;
+	String bairro;
+	String tipo;
+	double valor;
+	
+	public void reajuste (String tipo, double valor) {
+	     if (tipo == "casa") {
+	         valor = valor * 1.05;
+	        System.out.println("Valor reajustado a 5%: " + valor);
+	     }
+	     else {
+	         valor = valor * 1.08;
+	            System.out.println("Valor reajustado a 8%: " + valor);
+	     }
+	  }
+	
+    public void categoria (double num) {
+
+    if (num > 50000) {
+        System.out.println("Imóvel de classe A");
+    } if (num >= 10000 && num < 50000 ) {
+        System.out.println("Imóvel de classe B");
+    } if (num < 10000) {
+    	System.out.println("Imóvel de classe C");
+    }
+    }
+}
