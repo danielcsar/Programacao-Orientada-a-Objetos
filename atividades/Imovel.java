9dd384fdc19f3e4ecb2e89807298d3923700cd56
danielcsar <89037218+danielcsar@users.noreply.github.com>
2021-09-28 18:35:13 -0300

    Update Imovel.java

--- atividades/Imovel.java
+++ atividades/Imovel.java
@@ -7,24 +7,24 @@ public class Imovel {
 	double valor;
 	
 	public void reajuste (String tipo, double valor) {
-	     if (tipo == "casa") {
-	         valor = valor * 1.05;
-	        System.out.println("Valor reajustado a 5%: " + valor);
-	     }
-	     else {
-	         valor = valor * 1.08;
-	            System.out.println("Valor reajustado a 8%: " + valor);
-	     }
-	  }
+		if (tipo == "casa") {
+	        	valor = valor * 1.05;
+	        	System.out.println("Valor reajustado a 5%: " + valor);
+	     	}
+	     	else {
+	        	valor = valor * 1.08;
+	            	System.out.println("Valor reajustado a 8%: " + valor);
+	  	}
+	}
 	
-    public void categoria (double num) {
+	public void categoria (double num) {
 
-    if (num > 50000) {
-        System.out.println("Imóvel de classe A");
-    } if (num >= 10000 && num < 50000 ) {
-        System.out.println("Imóvel de classe B");
-    } if (num < 10000) {
-    	System.out.println("Imóvel de classe C");
-    }
-    }
+    		if (num > 50000) {
+        		System.out.println("ImÃ³vel de classe A");
+    		} if (num >= 10000 && num < 50000 ) {
+        		System.out.println("ImÃ³vel de classe B");
+    		} if (num < 10000) {
+    			System.out.println("ImÃ³vel de classe C");
+    		}
+    	}
 }
